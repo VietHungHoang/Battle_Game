0b37dfeaedb7f136142129ef3ecef330cec46804
VietHungHoang <viethunghoang1508@gmail.com>
2023-10-06 20:20:23 +0700

    Just Uploading

---
+++ fighter.py
@@ -0,0 +1,62 @@
+import pygame as pg
+
+class Fighter():
+	def __init__(self, x, y):
+		self.rect = pg.Rect((x, y, 80, 280))
+		self.vel_y = 0
+		self.jumping = False
+		self.attack_type = 0
+		self.attacking = False
+	def draw(self, surface):
+		pg.draw.rect(surface, 'red', self.rect)
+
+	def attack(self, surface, target): 
+		self.attacking = True
+		attacking_rect = pg.Rect((self.rect.centerx, self.rect.y, self.rect.width * 2, self.rect.height))
+		if attacking_rect.colliderect(target.rect):
+			print(1)
+
+		pg.draw.rect(surface,'green', attacking_rect)
+
+	def move(self, screen_width, screen_height, surface, target):
+		SPEED = 10
+		GRAVITY = 2
+		dx, dy = 0, 0
+
+
+		# get key pressed
+		key = pg.key.get_pressed()
+
+		# can only perform other actions if not currently attacking
+		if not self.attacking:
+			if key[pg.K_a]: dx = -SPEED
+			if key[pg.K_d]: dx = SPEED
+			if key[pg.K_w] and self.jumping == False:
+				self.vel_y = -30
+				self.jumping = True
+
+			if key[pg.K_r] or key[pg.K_t]:
+				self.attack(surface, target)
+				#determine which attack type was used
+				if key[pg.K_r]: self.attack_type = 1
+				if key[pg.K_t]: self.attack_type = 2
+
+		# apply garvity
+		self.vel_y += GRAVITY
+		dy += self.vel_y
+
+		# ensure player stays on screen
+		if self.rect.left + dx < 0: dx = -self.rect.left
+		if self.rect.left + dx > screen_width: dx = screen_width - self.rect.right
+		if self.rect.bottom + dy > screen_height - 110: # at floor
+			self.vel_y = 0
+			self.jumping = False
+			dy = screen_height - 110 - self.rect.bottom
+		# update coords
+		self.rect.x += dx
+		self.rect.y += dy
+
+
+		
+
+
---
+++ main.py
@@ -0,0 +1,38 @@
+import pygame as pg
+pg.init()
+from fighter import Fighter
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pg.display.set_caption("Battle Game")
+
+# set framerate
+clock = pg.time.Clock()
+FPS = 60
+
+#load background image
+bg_image = pg.image.load("assets/images/background/background.jpg").convert_alpha()
+
+fighter_1 = Fighter(200, 310)
+fighter_2 = Fighter(700, 310)
+
+def draw_bg():
+	scaled_bg = pg.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+	screen.blit(scaled_bg, (0, 0))
+
+running = True
+while running:
+	clock.tick(FPS)
+	draw_bg()
+	fighter_1.draw(screen)
+	fighter_2.draw(screen) 
+z
+	fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
+	# fighter_2.move()
+	for event in pg.event.get():
+		if event.type == pg.QUIT: running = False
+	pg.display.update()
+
+pg.quit()
+
